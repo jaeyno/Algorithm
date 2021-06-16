0b1b0d493fca1deb64daaaa98b6979fb7a2d4dc9
Jae No <jaeyno@gmail.com>
2021-06-15 22:01:16 -0400

    [Linked List] Find origin of loop of singly linked list

---
+++ FindLoopOfSinglyLinkedList.cs
@@ -0,0 +1,28 @@
+using System;
+
+//O(n) time | O(1) space
+public class Program {
+	public static LinkedList FindLoop(LinkedList head) {
+		LinkedList first = head.next;
+		LinkedList second = head.next.next;
+		while (first != second) {
+			first = first.next;
+			second = second.next.next;
+		}
+		first = head;
+		while (first != second) {
+			first = first.next;
+			second = second.next;
+		}
+		return first;
+	}
+
+	public class LinkedList {
+		public int value;
+		public LinkedList next = null;
+
+		public LinkedList(int value) {
+			this.value = value;
+		}
+	}
+}
