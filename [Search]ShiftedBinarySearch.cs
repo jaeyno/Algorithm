9586cc5b883381b40faf866b42473b7e737d9175
Jae No <jaeyno@gmail.com>
2021-11-25 22:40:33 -0500

    [Search] Shifted Binary Search Time Complexity: O(log(N))

---
+++ [Search]ShiftedBinarySearch.cs
@@ -0,0 +1,34 @@
+public class Program {
+    public static int ShiftedBinarySearch(int[] array, int target) {
+        return ShiftedBinarySearch(array, target, 0, array.Length - 1);
+    }
+
+    public static int ShiftedBinarySearch(int[] array, int target, int left, int right) {
+        //exit condition
+        if (left > right) {
+            return -1;
+        }
+
+        int middle = (left + right) / 2;
+        int potentialMatch = array[middle];
+        int leftNum = array[left];
+        int rightNum = array[right];
+
+        if (target == potentialMatch) {
+            return middle;
+        } else if (leftNum <= potentialMatch) {
+            if (target < potentialMatch && target >= leftNum) {
+                return ShiftedBinarySearch(array, target, left, middle - 1);
+            } else {
+                return ShiftedBinarySearch(array, target, middle + 1, right);
+            }
+        } else {
+            //when potentialMatch > leftNum
+            if (target > potentialMatch && target <= rightNum) {
+                return ShiftedBinarySearch(array, target, middle + 1, right);
+            } else {
+                return ShiftedBinarySearch(array, target, left, middle - 1);
+            }
+        }
+    }
+}
