28d78eebcfdf5ed47a9d49f0a1eeac2836ccc7ef
Jae No <jaeyno@gmail.com>
2022-01-20 08:02:13 -0500

    [Recursion] ProductSum

--- [Recursion]ProductSum.cs
+++ [Recursion]ProductSum.cs
@@ -5,13 +5,17 @@ public class Program {
 
     public static int ProductSumHelper(List<object> array, int multiplier) {
         int sum = 0;
-        foreach(object el in array) {
-            if (el is List<object>) {
-                sum += ProductSumHelper((List<object>)el, multiplier + 1);
+
+        foreach (object item in array) {
+            // check if the item is special array - array in array
+            if (item is List<object>) {
+                sum += ProductSumHelper((List<object>)item, multiplier + 1);
             } else {
-                sum += (int)el;
+                sum += (int)item;
             }
         }
+
         return sum * multiplier;
     }
+    //Time Complexity: O(n) | Space Complexity: O(n)
 }
