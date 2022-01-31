fdee8e4b5cdcf91785745ef3f024adc16958e6aa
Jae No <jaeyno@gmail.com>
2022-01-31 17:05:56 -0500

    [Heap] Minimum Heap Construction

--- [Heap]MinHeapConstruction.cs
+++ [Heap]MinHeapConstruction.cs
@@ -1,82 +1,79 @@
-using System;
-using System.Collections.Generic;
+public class MinHeap {
+	public List<int> heap = new List<int> ();
 
-// Do not edit the class below except for the buildHeap,
-// siftDown, siftUp, Peek, Remove, and Insert methods.
-// Feel free to add new properties and methods to the class.
-public class Program {
-	public class MinHeap {
-		public List<int> heap = new List<int>();
+	public MinHeap(List<int> array) {
+		heap = BuildMinHeap(array);
+	}
 
-		public MinHeap(List<int> array) {
-			heap = buildHeap(array);
+	//Time Complexity: O(n) | Space Complexity: O(1)
+	public List<int> BuildMinHeap(List<int> array) {
+		//Building a heap always starts from the most left & bottom parent node
+		//Find the most left & bottom paerent node from this equation, floor((i -1) / 2)
+		int parentIndex = (array.Count - 1 - 1) / 2;
+		for (int currentIndex = parentIndex; currentIndex >= 0; currentIndex--) {
+			SiftDown(currentIndex, array.Count - 1, array);
 		}
+		return array;
+	}
 
-		public List<int> buildHeap(List<int> array) {
-			// Write your code here.
-			int firstParentIndex = (array.Count - 2) / 2;
-			for (int currentIndex = firstParentIndex; currentIndex >= 0; currentIndex--) {
-				siftDown(currentIndex, array.Count - 1, array);
-			}
-			return array;
-		}
-
-		public void siftDown(int currentIndex, int endIndex, List<int> heap) {
-			//figure out the firstChild
-			int firstChildIndex = (currentIndex * 2) + 1;
-			//Iterate untill first child index reaches the end index
-			while (firstChildIndex <= endIndex) {
-				int secondChildIndex = (currentIndex * 2) + 2 <= endIndex ? (currentIndex * 2) + 2 : -1;
-				int indexToSwap;
-				if (secondChildIndex != -1 && heap[secondChildIndex] < heap[firstChildIndex]) {
-					indexToSwap = secondChildIndex;
-				} else {
-					indexToSwap = firstChildIndex;
-				}
-				if (heap[indexToSwap] < heap[currentIndex]) {
-					Swap(currentIndex, indexToSwap, heap);
-					currentIndex = indexToSwap;
-					firstChildIndex = (currentIndex * 2) + 1;
-				} else {
-					return;
-				}
-			}
-		}
-
-		public void siftUp(int currentIndex, List<int> heap) {
-			// Write your code here.
-			int parentIndex = (currentIndex - 1) / 2;
-			while (currentIndex > 0 && heap[currentIndex] < heap[parentIndex]) {
-				Swap(currentIndex, parentIndex, heap);
-				currentIndex = parentIndex;
-				parentIndex = (currentIndex - 1) /2;
-			}
-		}
-
-		public int Peek() {
-			// Write your code here.
-			return heap[0];
-		}
-
-		public int Remove() {
-			// Write your code here.
-			Swap(0, heap.Count - 1, heap);
-			int valueToRemove = heap[heap.Count - 1];
-			heap.RemoveAt(heap.Count - 1);
-			siftDown(0, heap.Count - 1, heap);
-			return valueToRemove;
-		}
-
-		public void Insert(int value) {
-			// Write your code here.
-			heap.Add(value);
-			siftUp(heap.Count - 1, heap);
-		}
+	//Time Complexity: O(log(n)) | Space Complexity: O(1)
+	public void SiftDown(int currentIndex, int endIndex, List<int> array) {
+		//Find the index of the first child node, (2 * i) + 1
+		int firstChildIndex = (2 * currentIndex) + 1;
 		
-		public void Swap(int i, int j, List<int> heap) {
-			int temp = heap[j];
-			heap[j] = heap[i];
-			heap[i] = temp;
+		//Iterate until firstChildIndex is out of the range
+		while (firstChildIndex <= endIndex) {
+			//Find the index of the second child node, (2 * i) + 2
+			int secondChildIndex = (2 * currentIndex) + 2 <= endIndex ? (2 * currentIndex) + 2 : -1;
+			int indexToSwap;
+			if (secondChildIndex != -1 && array[secondChildIndex] < array[firstChildIndex]) {
+				indexToSwap = secondChildIndex;
+			} else {
+				indexToSwap = firstChildIndex;
+			}
+
+			if (array[indexToSwap] < array[currentIndex]) {
+				Swap(currentIndex, indexToSwap, array);
+				currentIndex = indexToSwap;
+				firstChildIndex = (2 * currentIndex) + 1;
+			} else {
+				return;
+			}
 		}
+	}
+
+	//Time Complexity: O(log(n)) | Space Complexity: O(1)
+	public void SiftUp(int currentIndex, List<int> array) {
+		int parentIndex = (currentIndex - 1) / 2;
+		while (currentIndex > 0 && array[currentIndex] < array[parentIndex]) {
+			Swap(currentIndex, parentIndex, array);
+			currentIndex = parentIndex;
+			parentIndex = (currentIndex - 1) / 2;
+		}
+	}
+
+	//Swap the two index
+	public void Swap(int i, int j, List<int> array) {
+		int temp = array[j];
+		array[j] = array[i];
+		array[i] = temp;
+	}
+
+	//Peek the minimum value in the heap
+	public int Peek() {
+		return heap[0];
+	}
+
+	public int Remove() {
+		Swap(0, heap.Count - 1, heap);
+		int valueToRemove = heap[heap.Count - 1];
+		heap.RemoveAt(heap.Count - 1);
+		SiftDown(0, heap.Count -1, heap);
+		return valueToRemove;
+	}
+
+	public void Insert(int value) {
+		heap.Add(value);
+		SiftUp(heap.Count - 1, heap);
 	}
 }
