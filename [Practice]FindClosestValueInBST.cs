2584e8d370a0ca1f122acce77799754349be48a2
Jae No <jaeyno@gmail.com>
2022-03-22 14:18:18 -0400

    [Practice] Find Closest Value in BST

---
+++ [Practice]FindClosestValueInBST.cs
@@ -0,0 +1,31 @@
+public class Program {
+    public class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+
+    public int FindClosestValueInBST(BST tree, int target) {
+        return this.FindClosestValueInBST(tree, target, tree.value);
+    }
+
+    private int FindClosestValueInBST(BST tree, int target, int closest) {
+        //find the closest value in the tree
+        if (Math.Abs(target - closest) > Math.Abs(target - tree.value)) {
+            closest = tree.value;
+        }
+
+        //if target is smaller than tree value, go to left subtree else if target is greater than tree value, traverse to right subtree
+        if (target < tree.value && tree.left != null) {
+            return FindClosestValueInBST(tree.left, target, closest);
+        } else if (target > tree.value && tree.right != null) {
+            return FindClosestValueInBST(tree.right, target, closest);
+        } else {
+            return closest;
+        }
+    }
+}
