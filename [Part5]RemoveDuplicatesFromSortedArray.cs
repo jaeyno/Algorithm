784f6cf86ed1134168db0a918a1e1d1bde8a3efa
Jae No <jaeyno@gmail.com>
2022-04-08 14:02:41 -0400

    [Part3] Remove Duplicates From Array

---
+++ [Part5]RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        if (nums.Length == 0) return 0;
+
+        int i = 0;
+        for (int j = 0; j < nums.Length; j++) {
+            if (nums[i] == nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
